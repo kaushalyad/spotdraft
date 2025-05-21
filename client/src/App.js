7f3398d5e04a7ec72b5d19c346b2f20f8030541f
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2025-05-22 04:32:51 +0530

    fixed the issues

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import './i18n';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
