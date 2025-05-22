63a8292ccee3fc81420e37f8dbb60e1977b76fec
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2025-05-22 21:15:39 +0530

    fixed the issues

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+  origin: ['https://spotdraft-w59a.onrender.com', 'http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
+}));
+
+// ... rest of the existing code ... 
